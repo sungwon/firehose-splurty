9cb6717c1caac0557bf4a1f706b2a044f35fa1b9
Sungwon Choe <dokkaebi@gmail.com>
2016-01-19 20:52:28 +0000

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Splurty::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'quotes#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -53,4 +55,5 @@ Splurty::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
 end
